c07a9f9a8b06045a2f8f7d6cb39256c0ea3f9e7d
Maureen E <maureenekeugo1@gmail.com>
2024-08-20 19:34:57 -0400

    Added style property to disable link tag decoration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Shop from './Pages/Shop';
+import ShopCategoryy from './Pages/ShopCategoryy';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+
 
 function App() {
   return (
-    <div >
-      <Navbar/>
     
+    <div >
+
+      <BrowserRouter>
+
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/mens' element={<ShopCategoryy category='men'/>} />
+          <Route path='/womens' element={<ShopCategoryy category='women'/>} />
+          <Route path='/kids' element={<ShopCategoryy category='kid'/>} />
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/login' element={<LoginSignup/>} />
+
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import '../Navbar/Navbar.css'
 import logo from '../../Frontend_Assets/logo.png'
 import cart_icon from '../../Frontend_Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = () => {
@@ -13,23 +14,26 @@ const Navbar = () => {
 
 
     <div className="nav-logo">
+        
         <img src={logo} alt="" />    
-        <p>TrendCast </p>
+        <Link className="no-underline" to={'/'}><p>TrendCast</p></Link>
+        
     </div> 
 
 
     <ul className="nav-menu">
-        <li onClick={()=>{setMenu('Shop')}}>Shop{menu === 'Shop'?<hr></hr> :<></> }</li>    
-        <li onClick={()=>{setMenu('Men')}}>Men{menu === 'Men'?<hr></hr> :<></> }</li>    
-        <li onClick={()=>{setMenu('Women')}}>Women{menu === 'Women'?<hr></hr> :<></> }</li>    
-        <li onClick={()=>{setMenu('Kids')}}>Kids{menu === 'Kids'?<hr></hr> :<></> }</li>    
+        <Link className="no-underline" to={'/'}><li onClick={()=>{setMenu('Shop')}}>Shop{menu === 'Shop'?<hr></hr> :<></> }</li> </Link>
+        <Link className="no-underline" to={'/mens'}><li onClick={()=>{setMenu('Men')}}>Men{menu === 'Men'?<hr></hr> :<></> }</li></Link>
+        <Link className="no-underline" to={'/womens'}> <li onClick={()=>{setMenu('Women')}}>Women{menu === 'Women'?<hr></hr> :<></> }</li></Link>   
+        <Link className="no-underline" to={'/kids'}><li onClick={()=>{setMenu('Kids')}}>Kids{menu === 'Kids'?<hr></hr> :<></> }</li> </Link>
+           
 
     </ul> 
 
 
     <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <Link className="no-underline" to={'/login'}><button>Login</button></Link>
+        <Link className="no-underline" to={'/cart'}><img src={cart_icon} alt="" /></Link>
 
         <div className="nav-cart-count">0</div>
     </div> 
