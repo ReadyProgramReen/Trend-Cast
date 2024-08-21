6d2a5deff2b3bea8b21017307898910a8d53fd85
Maureen E <maureenekeugo1@gmail.com>
2024-08-20 20:10:01 -0400

    Added and styled Header conponents

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import '../Hero/Hero.css'
+import hand_icon from '../../Frontend_Assets/hand_icon.png'
+import arrow_icon from '../../Frontend_Assets/arrow.png'
+import hero_img from '../../Frontend_Assets/hero_image.png'
+
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+
+        {/* Left Side  */}
+        <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>new</p>
+                    <img src={hand_icon} alt="" />
+                </div>
+                <p>Collection</p>
+                <p>For Everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Latest Collection</div>
+                <img src={arrow_icon} alt="" />
+            </div>
+        </div>
+
+        {/* Right Side  */}
+        <div className="hero-right">
+            <img src={hero_img} alt="" />
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
