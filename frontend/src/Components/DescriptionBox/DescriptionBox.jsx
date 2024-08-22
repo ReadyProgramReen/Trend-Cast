035bb32ad9c97fda1b2b0e4f331c2c9e7bd1524f
Maureen E <maureenekeugo1@gmail.com>
2024-08-22 13:13:07 -0400

    Added and styled descriptionbox componet

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../DescriptionBox/DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionBox'>
+        <div className="descriptionbod-navigator">
+            <h3 className="descriptionbox-navbox">Description</h3>
+            <h3 className="descriptionbox-navbox fade">Reviews (124)</h3>                         
+        </div>
+
+        <div className="description-box-description">
+            <p>Discover timeless elegance with our classic white shirt. Designed with a clean, crisp look, this shirt is crafted from premium, breathable cotton that ensures comfort and durability. The tailored fit offers a sleek, sophisticated silhouette, while the button-down collar adds a touch of refinement. Perfect for both formal occasions and casual outings, this versatile shirt pairs effortlessly with your favorite jeans or dress pants. Easy to care for and stylish, it's a must-have staple in any wardrobe.</p>
+            <p>Whether you’re heading to the office or enjoying a weekend brunch, this classic white shirt is your go-to choice for effortless style. The timeless design and high-quality fabric ensure you look sharp and feel comfortable all day long. </p>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default DescriptionBox
